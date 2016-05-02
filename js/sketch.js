cda7461c77f4ac1c3ae90629cd171fb526dd5faa
Yining <ys2189@nyu.edu>
2016-05-02 11:28:31 -0400

    styled mouse box

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,9 @@
 function setup() {
   createCanvas(650, 600);
-  fill(204, 101, 192);
-  stroke(127, 63, 120);
-  // A rectangle
-  rect(40, 120, 120, 40);
-  // An ellipse
-  ellipse(240, 240, 80, 80);
-  // A triangle
-  triangle(300, 100, 320, 100, 310, 80);
+  fill(204,101,192);
+  stroke(127,63,120);
+  rect(40,120,120,40);
+  ellipse(240,240,80,80);
+  triangle(300,100,320,100,310,80);
 }
 function draw(){}
